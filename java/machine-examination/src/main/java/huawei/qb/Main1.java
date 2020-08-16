3195e029132d5f816ea9b0bcac120419cc0c3b43
gudujohn <jgc921204@foxmail.com>
2020-08-16 23:05:31 +0800

    add machine-examination project

---
+++ java/machine-examination/src/main/java/huawei/qb/Main1.java
@@ -0,0 +1,13 @@
+package huawei.qb;
+
+/**
+ * huawei.qb-Main
+ * 计算字符串最后一个单词的长度，单词以空格隔开。
+ *
+ * @classname: Main
+ * @description: 计算字符串最后一个单词的长度，单词以空格隔开。
+ * @author: JiangGengchao
+ * @date: 2020-08-15
+ **/
+public class Main {
+}
---
+++ java/machine-examination/src/main/java/huawei/qb/Main2.java
@@ -0,0 +1,13 @@
+package huawei.qb;
+
+/**
+ * huawei.qb-Main2
+ * 写出一个程序，接受一个由字母和数字组成的字符串，和一个字符，然后输出输入字符串中含有该字符的个数。不区分大小写。
+ *
+ * @classname: Main2
+ * @description: 写出一个程序，接受一个由字母和数字组成的字符串，和一个字符，然后输出输入字符串中含有该字符的个数。不区分大小写。
+ * @author: JiangGengchao
+ * @date: 2020-08-15
+ **/
+public class Main2 {
+}
