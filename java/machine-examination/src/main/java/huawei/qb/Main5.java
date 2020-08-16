7e58310d3a2e298662fa68cad45b2d477f18b212
gudujohn <jgc921204@foxmail.com>
2020-08-17 00:10:44 +0800

    add huawei main5

---
+++ java/machine-examination/src/main/java/huawei/qb/Main5.java
@@ -0,0 +1,13 @@
+package huawei.qb;
+
+/**
+ * huawei.qb-Main5
+ * 写出一个程序，接受一个十六进制的数，输出该数值的十进制表示。（多组同时输入 ）
+ *
+ * @classname: Main5
+ * @description: 写出一个程序，接受一个十六进制的数，输出该数值的十进制表示。（多组同时输入 ）
+ * @author: JiangGengchao
+ * @date: 2020-08-16
+ **/
+public class Main5 {
+}
