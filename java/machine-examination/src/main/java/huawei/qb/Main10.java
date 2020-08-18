9ee39b51c43d1a4ad2c2238d6d5aa38d1bb8652e
gudujohn <jgc921204@foxmail.com>
2020-08-18 13:26:25 +0800

    add Main

---
+++ java/machine-examination/src/main/java/huawei/qb/Main10.java
@@ -0,0 +1,13 @@
+package huawei.qb;
+
+/**
+ * huawei.qb-Main10
+ * 编写一个函数，计算字符串中含有的不同字符的个数。字符在ACSII码范围内(0~127)，换行表示结束符，不算在字符里。不在范围内的不作统计。多个相同的字符只计算一次
+ *
+ * @classname: Main10
+ * @description: 编写一个函数，计算字符串中含有的不同字符的个数。字符在ACSII码范围内(0~127)，换行表示结束符，不算在字符里。不在范围内的不作统计。多个相同的字符只计算一次
+ * @author: JiangGengchao
+ * @date: 2020-08-18
+ **/
+public class Main10 {
+}
--- java/machine-examination/src/main/java/huawei/qb/Main9.java
+++ java/machine-examination/src/main/java/huawei/qb/Main9.java
@@ -1,5 +1,7 @@
 package huawei.qb;
 
+import java.util.*;
+
 /**
  * huawei.qb-Main9
  * 输入一个int型整数，按照从右向左的阅读顺序，返回一个不含重复数字的新的整数。
@@ -10,4 +12,17 @@ package huawei.qb;
  * @date: 2020-08-17
  **/
 public class Main9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        StringBuilder sb = new StringBuilder(input);
+        sb.reverse();
+        Set<String> inputSet = new HashSet<>();
+        for(int i=0;i<sb.length();i++) {
+            String s = sb.substring(i, i+1);
+            if(inputSet.add(s)) {
+                System.out.print(s);
+            }
+        }
+    }
 }
